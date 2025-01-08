ab19c60b1bc90e4c7afd6772d0b9e3efff619711
Elliot Lee <drelliotlee@gmail.com>
2025-01-09 02:01:50 +0800

    switched from toggl Track to stopwatch app

--- adjust_windows.sh
+++ adjust_windows.sh
@@ -1,51 +1,31 @@
 #!/bin/bash
 
-# Wait for the desktop environment to fully load
-sleep 5
 
-# Open ChatGPT in Chrome
-google-chrome --new-window "https://chatgpt.com/" &
-sleep 2
 chatgpt_window=$(wmctrl -l | grep -i "ChatGPT" | awk '{print $1}')
 if [ -n "$chatgpt_window" ]; then
     wmctrl -i -r "$chatgpt_window" -e 0,0,0,1536,1587
 fi
 
-# Open a new Chrome Tab
-google-chrome --new-window &
-sleep 2
 new_tab_window=$(wmctrl -l | grep -i "New Tab - Google Chrome" | awk '{print $1}')
 if [ -n "$new_tab_window" ]; then
     wmctrl -i -r "$new_tab_window" -e 0,1536,0,1536,1726
 fi
 
-# Open Stopwatch
-/opt/google/chrome/google-chrome --profile-directory=Default --app-id=pdblaehbpalhkncehapjcegeogblkjdf &
-sleep 2
 toggl_window=$(wmctrl -l | grep -i "Stopwatch" | awk '{print $1}')
 if [ -n "$toggl_window" ]; then
     wmctrl -i -r "$toggl_window" -e 0,768,1588,768,140
 fi
 
-# Open Noisli - Favorites
-/opt/google/chrome/google-chrome --profile-directory=Default --app-id=gkgbglbaloldkpegmfbancknhcppcfao &
-sleep 2
 noisli_window=$(wmctrl -l | grep -i "Noisli - Favorites | Noisli" | awk '{print $1}')
 if [ -n "$noisli_window" ]; then
     wmctrl -i -r "$noisli_window" -e 0,0,1588,768,140
 fi
 
-# Open Visual Studio Code
-/usr/share/code/code &
-sleep 2
 vscode_window=$(wmctrl -l | grep -i "Visual Studio Code" | awk '{print $1}')
 if [ -n "$vscode_window" ]; then
     wmctrl -i -r "$vscode_window" -e 0,160,1750,1646,1700
 fi
 
-# Open Obsidian
-/home/elliot/Applications/Obsidian-1.7.7_436a792a92d0f402e2cec3f90fd22d44.AppImage --no-sandbox %U &
-sleep 2
 obsidian_window=$(wmctrl -l | grep -i "Obsidian" | awk '{print $1}')
 if [ -n "$obsidian_window" ]; then
     wmctrl -i -r "$obsidian_window" -e 0,1810,1728,1108,1726
@@ -53,4 +33,3 @@ fi
 
 # Ensure the script exits cleanly
 exit 0
-
--- monitor-setup.sh
+++ monitor-setup.sh
@@ -8,4 +8,3 @@ xrandr --output DP-2 --mode 3840x2160 --scale 0.8x0.8 --pos 0x0 --rotate normal
 
 # Configure laptop monitor (eDP-1) with calculated horizontal offset
 xrandr --output eDP-1 --mode 3456x2160 --scale 0.8x0.8 --pos 154x1728 --rotate normal
-
